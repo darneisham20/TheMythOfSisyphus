73f0b71a477ba1e68f6371ae6672f4fdca7e29e0
Zachary Himes <zacharymhimes@gmail.com>
2022-10-30 20:32:41 -0400

    did Booleans and Comparison operators

---
+++ csharpBasics/03_Operators/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// addition (+)
+
+// subtraction (-)
+
+// multiplication (*)
+
+// division (/)
+
+// remainder [Modulus, modulo] %
+
+//unary Operators
+// Ex: increment++ (counting up by one)
+//     decrement -- (counting down by one)
+
+// let's assume that student count holds the value of 30.
+int studentCount = 30;
+
+// add one to the studentCount
+//we can also use: studentCount+=1;
+//todo: this is adding one 'after the value'
+studentCount++;
+
+//todo: this is adding 'before the value'
+++studentCount;
+
+//decrement
+studentCount--;
+--studentCount;
+
+//addition (expression)
+int additionResult = 5+2;
+System.Console.WriteLine("5 + 2 = " + additionResult);
+
+//subtraction
+int subtractionResult = 5-2;
+System.Console.WriteLine("5 - 2 = " + subtractionResult);
+
+//multiplication
+int multiplicationResult = 5 * 2;
+System.Console.WriteLine("5* 2 = " + multiplicationResult);
+
+//division
+int divisionResult = 5 / 2;
+System.Console.WriteLine("5 / 2 = " + divisionResult);
+
+//Modulus
+System.Console.WriteLine(10%2);
+System.Console.WriteLine(10%9);
+System.Console.WriteLine(10%3);
+System.Console.WriteLine(10%5);
+
---
+++ csharpBasics/05_Booleans/Program.cs
@@ -0,0 +1,70 @@
+﻿//Booleans
+
+// can hold true / false
+
+bool isCool = false;
+isCool = true;
+
+//todo: Booleans and logical operators
+//Operators that perform logic
+//*    AND
+//*    OR
+//*    NOT
+
+//NOT
+//C# has a logical negation operator, 
+//or what we call the bang operator (!)
+// "!isCool" evaluates to the opposite of whatever value isCool is holding
+
+//AND 
+// C# uses ampersands (&) to indicate AND statements
+// A single (&) evaluates bothe conditions always
+// the (&&) only evaluate the right side if it is required
+
+//OR
+// C# uses (|) to idicate or statements
+//Similar to AND, one pipe checks both conditions
+// (||) will evaluate the first then the right
+
+//XOR
+// C# uses the caret character (^) indicates XOR statements.
+
+//--Boolean Operators
+
+//todo: Negation Operator
+bool isTrue = !false;
+bool isFalse = !true;
+
+//todo: And Operator
+ // true b/c BOTH HAVE TO BE TRUE!
+bool trueAndExample = true && true;
+bool falseAndExample = true && false; 
+//false b/c only one met conditions for true
+
+//todo: Or Operator
+bool trueOrExample = true || false;
+bool falseOrExample = false || false;
+
+//todo: XOR Operator
+bool trueExclusiveOr = true ^ false;
+bool falseExclusiveOR = true ^ true;
+
+//Examples
+Console.WriteLine($"NOT False = {isTrue}");
+System.Console.WriteLine($"False AND True = {falseAndExample}");
+System.Console.WriteLine($"True OR True = {trueOrExample}");
+System.Console.WriteLine($"True XOR True = {falseExclusiveOR}");
+
+//--Comparison Operator
+
+bool isGreaterThan = 10 > 10;
+bool isGreaterThanOrEqualTo = 10 >= 10;
+System.Console.WriteLine($"10 > 10 is {isGreaterThan}, and 10 >= 10 is {isGreaterThanOrEqualTo}");
+
+bool isLessThan = 5 < 5;
+bool isLessThanOrEqualTo = 5 <= 5;
+Console.WriteLine($"5 < 5 is {isLessThan}, while 5 <= 5 is {isLessThanOrEqualTo}");
+
+bool equality = 17 == 9;
+bool inequality = 17 != 9;
+Console.WriteLine($"17 == 9 and 17 != 9 are {equality} and {inequality}, respectively.");
